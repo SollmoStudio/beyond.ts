ebf6f7d4fbde3fe261d01e7870b08e257a8980f2
Seulgi Kim <dev@seulgi.kim>
2015-05-22 00:25:50 +0900

    Add more test cases.

--- test/future.ts
+++ test/future.ts
@@ -3,11 +3,49 @@ import Future = require('../lib/future');
 
 describe('Future', function () {
   describe('constructor', function () {
-    it('returns a Future object with a callback', function () {
+    it('returns a Future object with a callback', function (done) {
       let future = Future.create(function () {
         return;
       });
       assert.equal(future.constructor, Future);
+    });
+
+    it('returns a successful Future object with return value', function (done) {
+      let future = Future.create(function () {
+        return 10;
+      });
+      assert.equal(future.constructor, Future);
+      future.onSuccess(function (result: number) {
+        assert.equal(result, 10);
+        done();
+      });
+    });
+
+    it('returns a failed Future object when callback throws error', function (done) {
+      let future = Future.create(function () {
+        throw new Error('error');
+      });
+      assert.equal(future.constructor, Future);
+      future.onFailure(function (err: Error) {
+        assert.equal(err.message, 'error');
+        done();
+      });
+    });
+
+    it('creates an already completed successful future with the specified result.', function (done) {
+      let future = Future.successful('hello');
+      future.onSuccess(function (result: string) {
+        assert.equal(result, 'hello');
+        done();
+      });
+    });
+
+    it('creates an already completed failed future with the specified result.', function (done) {
+      let future = Future.failed(new Error('error'));
+      future.onFailure(function (err: Error) {
+        assert.equal(err.message, 'error');
+        done();
+      });
     });
   });
 
@@ -135,16 +173,6 @@ describe('Future', function () {
       );
       future.onFailure(function (err) {
         assert.equal(err.message, 'hello, error!');
-        done();
-      });
-    });
-  });
-
-  describe('#successful', function () {
-    it('creates an already completed successful future with the specified result.', function (done) {
-      let future = Future.successful('hello');
-      future.onSuccess(function (result: string) {
-        assert.equal(result, 'hello');
         done();
       });
     });
