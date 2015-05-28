405bf53c585ac2064c61870582101ed747c99ab4
Seulgi Kim <dev@seulgi.kim>
2015-05-29 01:22:17 +0900

    Validate the default value when creating Schema.

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -1,4 +1,45 @@
+import _ = require('underscore');
+import util = require('util');
 import Option = require('./schema/option');
+import Type = require('./schema/type');
+import ObjectId = require('./object-id');
+
+function isInteger(value: number): boolean {
+  return _.isNumber(value) && value % 1 === 0;
+}
+
+function isObjectId(value: any): boolean {
+  return value && value.constructor && value.constructor === ObjectId;
+}
+
+const checkers: { [type: string]: (value: any) => boolean } = {
+  [Type.boolean]: _.isBoolean,
+  [Type.integer]: isInteger,
+  [Type.float]: _.isNumber,
+  [Type.string]: _.isString,
+  [Type.date]: _.isDate,
+  [Type.array]: _.isArray,
+  [Type.embedding]: (value: any) => { return true; },
+  [Type.objectId]: isObjectId
+};
+
+function checkType(value: any, type: Type): boolean {
+  if (_.isNull(value)) {
+    return true;
+  }
+  if (_.isUndefined(value)) {
+    return true;
+  }
+
+  return checkers[type](value);
+}
+
+function validateOption(option: Option, name: string): boolean {
+  if (!checkType(option.default, option.type)) {
+    throw new Error(util.format('default value of %s(%j) is not %s.', name, option.default, Type[option.type]));
+  }
+  return true;
+}
 
 class Schema {
   private version: number;
@@ -6,6 +47,7 @@ class Schema {
 
   constructor(version: number, fields: { [name: string]: Option }) {
     this.version = version;
+    _.map(fields, validateOption);
     this.fields = fields;
   }
 }
