f743e043f630801cb7ee8cf70862cb4defe92ddb
Seulgi Kim <dev@seulgi.kim>
2015-06-01 11:17:16 +0900

    Initialize db before creating Collection.
    The next patch will make Collection use db connection on constructor.

--- test/db/collection.ts
+++ test/db/collection.ts
@@ -1,18 +1,32 @@
 import assert = require('assert');
 import Schema = require('../../lib/db/schema');
 import Type = require('../../lib/db/schema/type');
+import connection = require('../../lib/db/connection');
 import db = require('../../lib/db');
 
 describe('db.collection', () => {
   describe('#constructor', () => {
-    it('create Collection with schema', () => {
-      let userSchema = new Schema(1, {
-        firstName: { type: Type.string },
-        lastName: { type: Type.string },
-        age: { type: Type.integer }
+    it('create Collection with schema', (done: MochaDone) => {
+      connection.initialize('mongodb://localhost:27017/beyondTest')
+      .onSuccess(() => {
+        let userSchema = new Schema(1, {
+          firstName: { type: Type.string },
+          lastName: { type: Type.string },
+          age: { type: Type.integer }
+        });
+        let userCollection = new db.Collection("beyond.user", userSchema);
+        assert.equal(userCollection.constructor, db.Collection);
+
+        connection.close(true)
+        .onSuccess(() => {
+          done();
+        }).onFailure((err: Error) => {
+          done(err);
+        });
+      })
+      .onFailure((err: Error) => {
+        done(err);
       });
-      let userCollection = new db.Collection("beyond.user", userSchema);
-      assert.equal(userCollection.constructor, db.Collection);
     });
   });
 });
