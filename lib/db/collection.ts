3f1b381a4a9c66ad697d4e4522e566de9cee94e6
Seulgi Kim <dev@seulgi.kim>
2015-06-01 12:14:06 +0900

    Implement insert to insert a document.

--- lib/db/collection.ts
+++ lib/db/collection.ts
@@ -1,10 +1,14 @@
+import Future = require('sfuture');
 import _ = require('underscore');
+import mongodb = require('mongodb');
 import Field = require('./field');
 import Schema = require('./schema');
+import connection = require('./connection');
 
 class Collection {
   private name: string;
   private fields: Field<any>[];
+  private collection: mongodb.Collection;
 
   constructor(name: string, schema: Schema, option?: any) {
     this.name = name;
@@ -13,6 +17,13 @@ class Collection {
     if (!_.isUndefined(option)) {
       console.warn('You use option argument of collection(%s), option argument of Collection constructor is not implemented yet.', name);
     }
+
+    this.collection = connection.connection().collection(name);
+  }
+
+  insert (document: any): Future<any> {
+    // TODO: validation with schema.
+    return Future.denodify(this.collection.insert, this.collection, document);
   }
 }
 
--- test/db/collection.ts
+++ test/db/collection.ts
@@ -1,3 +1,4 @@
+import Future = require('sfuture');
 import assert = require('assert');
 import Schema = require('../../lib/db/schema');
 import Type = require('../../lib/db/schema/type');
@@ -33,6 +34,33 @@ describe('db.collection', () => {
       });
       let userCollection = new db.Collection("beyondTestCollection", userSchema);
       assert.equal(userCollection.constructor, db.Collection);
+    });
+  });
+
+  describe('#insert', () => {
+    it('insert one document.', (done) => {
+      let userSchema = new Schema(1, {
+        firstName: { type: Type.string },
+        lastName: { type: Type.string },
+        age: { type: Type.integer }
+      });
+      let userCollection = new db.Collection("beyondTestCollection", userSchema);
+      assert.equal(userCollection.constructor, db.Collection);
+
+      let document = {'firstName': 'name', 'lastName': 'last', age: 20};
+      userCollection
+      .insert(document)
+      .flatMap(() => {
+        let mongoConnection = connection.connection();
+        let collection = mongoConnection.collection('beyondTestCollection');
+        let cursor = collection.find({});
+        return Future.denodify(cursor.toArray, cursor);
+      }).map((docs: any[]) => {
+        assert.equal(docs.length, 1);
+        assert.deepEqual(docs[0], document);
+        return docs;
+      })
+      .nodify(done);
     });
   });
 });
