c929dffb4ba70130085a17538ac173cf2af9726b
Seulgi Kim <dev@seulgi.kim>
2015-06-01 11:48:47 +0900

    Extract db connect and close to util.
    And call then before and after the test.

--- test/db/collection.ts
+++ test/db/collection.ts
@@ -3,30 +3,26 @@ import Schema = require('../../lib/db/schema');
 import Type = require('../../lib/db/schema/type');
 import connection = require('../../lib/db/connection');
 import db = require('../../lib/db');
+import util = require('./util');
 
 describe('db.collection', () => {
-  describe('#constructor', () => {
-    it('create Collection with schema', (done: MochaDone) => {
-      connection.initialize('mongodb://localhost:27017/beyondTest')
-      .onSuccess(() => {
-        let userSchema = new Schema(1, {
-          firstName: { type: Type.string },
-          lastName: { type: Type.string },
-          age: { type: Type.integer }
-        });
-        let userCollection = new db.Collection("beyond.user", userSchema);
-        assert.equal(userCollection.constructor, db.Collection);
+  before((done: MochaDone) => {
+    util.connect(done);
+  });
 
-        connection.close(true)
-        .onSuccess(() => {
-          done();
-        }).onFailure((err: Error) => {
-          done(err);
-        });
-      })
-      .onFailure((err: Error) => {
-        done(err);
+  after((done: MochaDone) => {
+    util.close(true, done);
+  });
+
+  describe('#constructor', () => {
+    it('create Collection with schema', () => {
+      let userSchema = new Schema(1, {
+        firstName: { type: Type.string },
+        lastName: { type: Type.string },
+        age: { type: Type.integer }
       });
+      let userCollection = new db.Collection("beyond.user", userSchema);
+      assert.equal(userCollection.constructor, db.Collection);
     });
   });
 });
