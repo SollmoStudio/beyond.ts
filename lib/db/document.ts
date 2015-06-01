877feb8a58702df680e7da49d15c234eba068a73
Seulgi Kim <dev@seulgi.kim>
2015-06-02 00:45:42 +0900

    Add setter and getter for document.

--- lib/db/document.ts
+++ lib/db/document.ts
@@ -6,13 +6,32 @@ class DbDocument {
   private _doc: any;
   private collection: Collection;
   private fields: { [name: string]: Field<any> };
+  private updatedValues: { [name: string]: any };
 
   constructor(doc: any, collection: Collection) {
     this._doc = doc;
     this.collection = collection;
+    this.fields = { };
+    this.updatedValues = { };
 
     _.map(collection.fields, (field: Field<any>) => {
-      this.fields[field.name()] = field;
+      let fieldName = field.name();
+      this.fields[fieldName] = field;
+
+      Object.defineProperty(this, fieldName, {
+        enumerable: true,
+        value: (value?: any) => {
+          if (_.isUndefined(value)) {
+            let updatedValue = this.updatedValues[fieldName];
+            if (_.isUndefined(updatedValue)) {
+              return this._doc[fieldName];
+            }
+            return updatedValue;
+          } else {
+            this.updatedValues[fieldName] = value;
+          }
+        }
+      });
     });
   }
 
--- test/db/document.ts
+++ test/db/document.ts
@@ -2,6 +2,7 @@ import assert = require('assert');
 import mongodb = require('mongodb');
 import Document = require('../../lib/db/document');
 import Schema = require('../../lib/db/schema');
+import Type = require('../../lib/db/schema/type');
 import db = require('../../lib/db');
 import util = require('./util');
 
@@ -35,5 +36,43 @@ describe('#document', () => {
     let rawDoc = { _id: oid, array: [ 1, 2, 3 ], name: 'string', num: 4 };
     let doc = new Document(rawDoc, testCollection);
     assert.equal(JSON.stringify(rawDoc), JSON.stringify(doc));
+  });
+
+  it('Document getter function return original value if not modified.', () => {
+    let testSchema = new Schema(1, { array: { type: Type.array, elementType: { type: Type.integer } }, name: { type: Type.string }, num: { type: Type.integer } });
+    let testCollection = new db.Collection("beyondTestCollection", testSchema);
+    assert.equal(testCollection.constructor, db.Collection);
+
+    let hexString = "abcdef0123456789abcdef01";
+    let oid = new mongodb.ObjectID(hexString);
+    let rawDoc = { _id: oid, array: [ 1, 2, 3 ], name: 'string', num: 4 };
+    let doc: any = new Document(rawDoc, testCollection);
+
+    assert.equal(doc.name(), rawDoc.name);
+    assert.equal(doc.num(), rawDoc.num);
+    assert.deepEqual(doc.array(), rawDoc.array);
+  });
+
+  it('Document getter function return updated value if not modified.', () => {
+    let testSchema = new Schema(1, { array: { type: Type.array, elementType: { type: Type.integer } }, name: { type: Type.string }, num: { type: Type.integer } });
+    let testCollection = new db.Collection("beyondTestCollection", testSchema);
+    assert.equal(testCollection.constructor, db.Collection);
+
+    let hexString = "abcdef0123456789abcdef01";
+    let oid = new mongodb.ObjectID(hexString);
+    let rawDoc = { _id: oid, array: [ 1, 2, 3 ], name: 'string', num: 4 };
+    let doc: any = new Document(rawDoc, testCollection);
+
+    assert.equal(doc.name(), rawDoc.name);
+    assert.equal(doc.num(), rawDoc.num);
+    assert.deepEqual(doc.array(), rawDoc.array);
+
+    let newName = 'new name';
+    let newArray = [ 1, 2, 3, 4, 5 ];
+    doc.name(newName);
+    assert.equal(doc.name(), newName);
+    assert.equal(doc.num(), rawDoc.num);
+    doc.array(newArray);
+    assert.deepEqual(doc.array(), newArray);
   });
 });
