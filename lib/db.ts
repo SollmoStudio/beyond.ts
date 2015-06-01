01dd2f592469a198c63810ef412aaafa0b02780b
Seulgi Kim <dev@seulgi.kim>
2015-06-01 10:47:52 +0900

    Expose connection initialize and close on db.

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,15 @@
+import Future = require('sfuture');
+import connection = require('./db/connection');
+
 export const Collection = require('./db/collection');
 
 export const ASC = 1;
 export const DESC = -1;
+
+export function initialize(url: string): Future<void> {
+  return connection.initialize(url);
+}
+
+export function close(forceClose: boolean): Future<void> {
+  return connection.close(forceClose);
+}
