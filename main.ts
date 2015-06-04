ab2132d8527f1049d0ced4ae7ba44cc6db3a4af7
Seulgi Kim <dev@seulgi.kim>
2015-06-04 21:46:05 +0900

    Initialize db on start and close at exit.

--- main.ts
+++ main.ts
@@ -1,11 +1,32 @@
 import bodyParser = require('body-parser');
 import express = require('express');
+import db = require('./core/db');
 import plugin = require('./core/plugin');
+
+let appConfig = require('./config/app');
 
 let app = express();
 
-(() => {
-  plugin.initialize();
+plugin.initialize();
+db.initialize(appConfig.mongodb.url)
+.onSuccess(() => {
+  let exitHandler = () => {
+    console.log('Closing db connection.');
+    db.close(true);
+  };
+  let signalExit = (sig: string) => {
+    return () => {
+      console.log('Process terminated by ' + sig);
+      process.exit(0);
+    };
+  };
+  process.on('SIGINT', signalExit('SIGINT'));
+  process.on('SIGTERM', signalExit('SIGTERM'));
+  process.on('exit', exitHandler);
+  process.on('uncaughtException', (ex: Error) => {
+    console.error('Uncaught exception %j', ex);
+    exitHandler();
+  });
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended: false}));
@@ -26,4 +47,4 @@ let app = express();
 
     console.log('Example app listening at http://%s:%s', host, port);
   });
-})();
+});
