359fd27ec9b84eee553287b44665895a1fcf2c6f
Seulgi Kim <dev@seulgi.kim>
2015-06-04 21:28:29 +0900

    Indent code.
    It just indents code to prepare the next patch.

--- main.ts
+++ main.ts
@@ -4,24 +4,26 @@ import plugin = require('./core/plugin');
 
 let app = express();
 
-plugin.initialize();
+(() => {
+  plugin.initialize();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+  app.get('/', function (req, res) {
+    res.send('Hello World!');
+  });
 
-function handlePlugin(req: express.Request, res: express.Response) {
-  plugin.get(req.params.name).handle(req, res);
-}
-app.get('/plugin/:name/:action(*)', handlePlugin);
-app.post('/plugin/:name/:action(*)', handlePlugin);
+  function handlePlugin(req: express.Request, res: express.Response) {
+    plugin.get(req.params.name).handle(req, res);
+  }
+  app.get('/plugin/:name/:action(*)', handlePlugin);
+  app.post('/plugin/:name/:action(*)', handlePlugin);
 
-let server = app.listen(9000, function () {
-  let host = server.address().address;
-  let port = server.address().port;
+  let server = app.listen(9000, function () {
+    let host = server.address().address;
+    let port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+    console.log('Example app listening at http://%s:%s', host, port);
+  });
+})();
