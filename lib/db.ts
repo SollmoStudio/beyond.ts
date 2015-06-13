38c9d281d7d55c4bc304f51c268117eb4ca72f1b
Seulgi Kim <dev@seulgi.kim>
2015-06-13 12:26:23 +0900

    Stop to exports Collection, Schema and query in db.
    Currently, Collection can be accessed by both of require('db/collection') and
    require('db').Collection.
    This patch makes it just can be accessed by require('db/collection').

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,5 @@
 import mongodb = require('mongodb');
 
-export const Collection = require('../core/db/collection');
-export const Schema = require('../core/db/schema');
-export const query = require('../core/db/query');
-
 export const ASC = 1;
 export const DESC = -1;
 
